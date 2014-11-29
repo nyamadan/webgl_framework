38ffc2d52361568b60d066bdfa900dc1821c0230
Yoshiki Yamada <nyamadandan@gmail.com>
2014-11-30 08:26:51 +0900

    import math

--- lib/webgl_framework.dart
+++ lib/webgl_framework.dart
@@ -8,9 +8,9 @@ import "dart:math" as Math;
 
 import "package:vector_math/vector_math.dart";
 
+part "math.dart";
 part "trackball.dart";
 part "webgl_buffer.dart";
 part "webgl_exception.dart";
 part "webgl_texture.dart";
 part "webgl_renderer.dart";
-
