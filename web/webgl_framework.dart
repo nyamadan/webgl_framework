33cf54f6de616f996aacb06228232bb0be1439f4
Yoshiki Yamada <nyamadandan@gmail.com>
2014-11-30 13:42:47 +0900

    initialize logger

--- web/webgl_framework.dart
+++ web/webgl_framework.dart
@@ -1,8 +1,15 @@
 import 'dart:html';
+import "package:logging/logging.dart";
 
 import "mmd_renderer.dart";
 
 void main() {
+  //initialize logger
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((LogRecord rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+  });
+
   var renderer = new MMD_Renderer();
 
   document.querySelector("body").append(renderer.dom);
