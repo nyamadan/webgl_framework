8b296148b1f329cb7932fccbbce9702e5c411404
Yoshiki Yamada <nyamadandan@gmail.com>
2014-12-23 16:23:23 +0900

    add byte per element

--- lib/webgl_buffer.dart
+++ lib/webgl_buffer.dart
@@ -3,11 +3,13 @@ part of webgl_framework;
 class WebGLBuffer {
   TypedData data;
   GL.Buffer buffer;
+  int byte_per_element;
 }
 
 class WebGLArrayBuffer32 implements WebGLBuffer {
   Float32List data;
   GL.Buffer buffer;
+  int byte_per_element;
 
   WebGLArrayBuffer32(GL.RenderingContext gl, Float32List data) {
     var vbo = gl.createBuffer();
@@ -24,14 +26,41 @@ class WebGLArrayBuffer32 implements WebGLBuffer {
   }
 }
 
-class WebGLElementArrayBuffer16 {
-  Uint16List data;
+class WebGLElementArrayBuffer8 implements WebGLBuffer {
+  Uint8List data;
   GL.Buffer buffer;
-  WebGLElementArrayBuffer16(GL.RenderingContext gl, Uint16List data) {
+  int byte_per_element;
+
+  WebGLElementArrayBuffer8(GL.RenderingContext gl, Uint8List data) {
+    this.byte_per_element = Uint8List.BYTES_PER_ELEMENT;
+
     var ibo = gl.createBuffer();
     this.buffer = ibo;
     this.setData(gl, data);
   }
+
+  void setData(GL.RenderingContext gl, Uint8List data) {
+    gl.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.buffer);
+    gl.bufferDataTyped(GL.ELEMENT_ARRAY_BUFFER, data, GL.STATIC_DRAW);
+    gl.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, null);
+
+    this.data = data;
+  }
+}
+
+class WebGLElementArrayBuffer16 implements WebGLBuffer {
+  Uint16List data;
+  GL.Buffer buffer;
+  int byte_per_element;
+
+  WebGLElementArrayBuffer16(GL.RenderingContext gl, Uint16List data) {
+    this.byte_per_element = Uint16List.BYTES_PER_ELEMENT;
+
+    var ibo = gl.createBuffer();
+    this.buffer = ibo;
+    this.setData(gl, data);
+  }
+
   void setData(GL.RenderingContext gl, Uint16List data) {
     gl.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.buffer);
     gl.bufferDataTyped(GL.ELEMENT_ARRAY_BUFFER, data, GL.STATIC_DRAW);
@@ -41,10 +70,14 @@ class WebGLElementArrayBuffer16 {
   }
 }
 
-class WebGLElementArrayBuffer32 {
+class WebGLElementArrayBuffer32 implements WebGLBuffer {
   Uint32List data;
   GL.Buffer buffer;
+  int byte_per_element;
+
   WebGLElementArrayBuffer32(GL.RenderingContext gl, Uint32List data) {
+    this.byte_per_element = Uint32List.BYTES_PER_ELEMENT;
+
     var ibo = gl.createBuffer();
     this.buffer = ibo;
     this.setData(gl, data);
