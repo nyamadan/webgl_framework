c9e1416ea5052e8f574d839b69a01d407e934076
Yoshiki YAMADA <nyamadandan@gmail.com>
2015-01-01 23:07:31 +0900

    add log level

--- web/webgl_framework.dart
+++ web/webgl_framework.dart
@@ -7,7 +7,20 @@ void main() {
   //initialize logger
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((LogRecord rec) {
-    print('${rec.level.name}: ${rec.time}: ${rec.message}');
+    if(rec.level >= Level.SEVERE) {
+      window.console.log('${rec.level.name}: ${rec.time}: ${rec.message}');
+      return;
+    }
+
+    if(rec.level >= Level.WARNING) {
+      window.console.warn('${rec.level.name}: ${rec.time}: ${rec.message}');
+      return;
+    }
+    if(rec.level >= Level.INFO) {
+      window.console.info('${rec.level.name}: ${rec.time}: ${rec.message}');
+      return;
+    }
+    window.console.log('${rec.level.name}: ${rec.time}: ${rec.message}');
   });
   var renderer = new TeapotRenderer(512, 512);
 
