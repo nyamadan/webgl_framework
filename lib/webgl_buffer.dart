920fb8f387b24ceaa948a9697de24488fcc8522e
Yoshiki YAMADA <nyamadandan@gmail.com>
2015-01-01 23:06:45 +0900

    add sub data

--- lib/webgl_buffer.dart
+++ lib/webgl_buffer.dart
@@ -6,12 +6,22 @@ class WebGLBuffer {
   int byte_per_element;
 }
 
+class SubArrayData {
+  Float32List data;
+  int offset;
+  int byte_per_element = 4;
+  
+  SubArrayData(this.offset, this.data);
+}
+
 class WebGLArrayBuffer32 implements WebGLBuffer {
   Float32List data;
   GL.Buffer buffer;
   int byte_per_element;
 
   WebGLArrayBuffer32(GL.RenderingContext gl, Float32List data) {
+    this.byte_per_element = 4;
+    
     var vbo = gl.createBuffer();
     this.buffer = vbo;
     this.setData(gl, data);
@@ -20,6 +30,14 @@ class WebGLArrayBuffer32 implements WebGLBuffer {
   void setData(GL.RenderingContext gl, Float32List data) {
     gl.bindBuffer(GL.ARRAY_BUFFER, this.buffer);
     gl.bufferDataTyped(GL.ARRAY_BUFFER, data, GL.STATIC_DRAW);
+    gl.bindBuffer(GL.ARRAY_BUFFER, null);
+
+    this.data = data;
+  }
+  
+  void setSubData(GL.RenderingContext gl, SubArrayData sub_data) {
+    gl.bindBuffer(GL.ARRAY_BUFFER, this.buffer);
+    gl.bufferSubDataTyped(GL.ARRAY_BUFFER, sub_data.offset, sub_data.data);
     gl.bindBuffer(GL.ARRAY_BUFFER, null);
 
     this.data = data;
