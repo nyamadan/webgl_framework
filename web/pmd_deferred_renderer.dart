346d37be90c47db06297e1638fc9afe74e3ddd17
Yoshiki Yamada <nyamadandan@gmail.com>
2015-01-11 22:43:06 +0900

    implement sraa

--- web/pmd_deferred_renderer.dart
+++ web/pmd_deferred_renderer.dart
@@ -20,13 +20,57 @@ class PMD_DeferredRenderer extends WebGLRenderer {
   uniform sampler2D normal_texture;
   uniform sampler2D depth_texture;
 
+  uniform vec2 delta;
+
   varying vec2 v_coord;
 
+  const float scale = 10.0;
+  const float depth_scale = 1.0;
+  float bilateral(vec3 cn, float cz, vec3 n, float z) {
+    return exp(-scale * max(depth_scale * abs(cz - z), 1.0 - dot(cn, n)));
+  }
+
   void main(void){
-    vec4 color = texture2D(color_texture, v_coord);
-    vec4 normal = texture2D(normal_texture, v_coord);
-    float depth = texture2D(depth_texture, v_coord).r;
-    gl_FragColor = vec4(color.rgb, 1.0);
+    vec3 result = vec3(0.0);
+
+    mat3 weights = mat3(0.0);
+    for(int i1 = 0; i1 < 2; i1++) {
+      for(int j1 = 0; j1 < 2; j1++) {
+        vec2 offset1 = vec2(delta.x * float(i1), delta.y * float(j1));
+        mat3 tmp_weights = mat3(0.0);
+        vec3 cn = texture2D(normal_texture, v_coord + offset1).rgb * 2.0 - vec3(1.0);;
+        float cz = texture2D(depth_texture, v_coord + offset1).r;
+        float sum = 0.0;
+        for(int i2 = -1; i2 <= 1; i2++) {
+          for(int j2 = -1; j2 <= 1; j2++) {
+            if(abs(float(i1 + i2)) <= 1.0 && abs(float(j1 + j2)) <= 1.0) {
+              vec2 offset2 = offset1 + vec2(delta.x * float(i2), delta.y * float(j2));
+              vec3 n = texture2D(normal_texture, v_coord + offset2).rgb * 2.0 - vec3(1.0);
+              float z = texture2D(depth_texture, v_coord + offset2).r;
+  
+              float w = bilateral(cn, cz, n, z);
+              tmp_weights[i1 + i2 + 1][j1 + j2 + 1] = w;
+              sum += w;
+            }
+          }
+        }
+
+        for(int i = 0; i < 3; i++) {
+          for(int j = 0; j < 3; j++) {
+            weights[i][j] += tmp_weights[i][j] / sum;
+          }
+        }
+      }
+    }
+
+    for(int i = 0; i < 3; i++) {
+      for(int j = 0; j < 3; j++) {
+        vec2 offset = vec2(delta.x * float(i - 1), delta.y * float(j - 1));
+        result += weights[i][j] * 0.25 *  texture2D(color_texture, v_coord + offset).rgb;
+      }
+    }
+
+    gl_FragColor = vec4(result, 1.0);
   }
   """;
 
@@ -57,6 +101,7 @@ class PMD_DeferredRenderer extends WebGLRenderer {
       "color_texture",
       "normal_texture",
       "depth_texture",
+      "delta",
     ]);
 
     this.position_buffer = new WebGLArrayBuffer32(gl, new Float32List.fromList([
@@ -92,6 +137,8 @@ class PMD_DeferredRenderer extends WebGLRenderer {
     gl.useProgram(this.program);
     
     gl.enable(GL.DEPTH_TEST);
+    
+    this.setUniformVector2("delta", new Vector2(1.0 / this.dom.width, 1.0 / this.dom.height));
 
     this.setUniformTexture0("color_texture", this.color_texture);
     this.setUniformTexture1("normal_texture", this.normal_texture);
